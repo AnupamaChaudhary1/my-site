f503c0f4cc60722612c7b164f622bf453df10e8c
Anupama Chaudhary <121998531+AnupamaChaudhary1@users.noreply.github.com>
2025-07-15 19:12:17 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -45,6 +45,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: '/my-site/',           // <-- add this line with your repo name!
   plugins: [react()],
   resolve: {
     alias: {
@@ -52,3 +53,4 @@ export default defineConfig({
     },
   },
 });
+
