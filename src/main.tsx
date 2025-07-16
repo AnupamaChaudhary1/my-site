64ac0019ca32cee68945a6bfc92ab98d122e5ac3
Anupama Chaudhary <121998531+AnupamaChaudhary1@users.noreply.github.com>
2025-07-16 18:40:41 +0545

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-site/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
